72f3aa519c751f3a9852a9ad6730e35b88193e38
Philipp@Rambacher.net <Philipp@Rambacher.net>
2024-02-08 19:25:32 +0100

    New_Game_Button_but_it_dont_work

--- Hangman_V2.py
+++ Hangman_V2.py
@@ -4,11 +4,6 @@ from string import ascii_uppercase
 import random
 
 my_datei = open("Hangman_words.txt", "r", encoding="UTF8")
-eingegebene_Buchstaben = []
-correct_guess = 0
-max_versuche = 12
-int_versuche = 0
-guess_word = ""
 data = my_datei.read().splitlines()
 GUI = Tk()
 GUI.title("Hangman")
@@ -19,6 +14,7 @@ photos = [PhotoImage(file="images/hang0.png"), PhotoImage(file="images/hang1.png
           PhotoImage(file="images/hang9.png"), PhotoImage(file="images/hang10.png"), PhotoImage(file="images/hang11.png")]
 
 def Choose_Word(data):
+    global Hangman_Word
     int_Rows_datei = len(data)
     Random_int = random.randint(0, int_Rows_datei - 1)
     Hangman_Word = data[Random_int]
@@ -34,13 +30,21 @@ def create_underscore_String(Len_word):
     word_label.grid(row=3, column=0, columnspan=9)
     return Word_with_blanks
 
-Hangman_Word = Choose_Word(data)
-Len_word = len(Hangman_Word)
-Word_with_blanks = create_underscore_String(Len_word)
+def newGame():
+    global Hangman_Word
+    global Len_word
+    global Word_with_blanks
+    global int_versuche
+    global correct_guess
+    correct_guess = 0
+    int_versuche = 0
+    Hangman_Word = Choose_Word(data)
+    Len_word = len(Hangman_Word)
+    Word_with_blanks = create_underscore_String(Len_word)
 
 def make_guess(Letter):
-    global guess_word, correct_guess, int_versuche, Underscore_String
-    if int_versuche < 11 and correct_guess < Len_word:
+    global correct_guess, int_versuche, Underscore_String, Word_with_blanks
+    if int_versuche < 11:
         if Letter in Hangman_Word:
             index_counter = 0
             for One_Letter in Hangman_Word:
@@ -58,6 +62,7 @@ def make_guess(Letter):
             imgLabel.config(image=photos[int_versuche])
     else:
         messagebox.showinfo("Verloren!", f"Du hast das Wort: '{Hangman_Word}' NICHT erraten.")
+
 imgLabel = Label(GUI)
 imgLabel.grid(row=0, column=0, columnspan=3, padx=10, pady=40)
 n = 0
@@ -65,4 +70,8 @@ for c in ascii_uppercase:
     Button(GUI, text=c, command=lambda c=c: make_guess(c), font=('Helvetica 18'), width=4).grid(row=5 + n // 9, column=n % 9)
     n += 1
 
+new_game_button = Button(GUI, text="Neues\nSpiel", command=lambda: newGame(), font=("Helvetica 10 bold"))
+new_game_button.grid(row=5 + n // 9, column=n % 9) 
+
+newGame()
 GUI.mainloop()
