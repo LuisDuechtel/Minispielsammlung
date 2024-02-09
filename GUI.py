5698b3369b43b024e2e349df0fca686a96f68ef4
Philipp@Rambacher.net <Philipp@Rambacher.net>
2024-02-09 17:40:37 +0100

    GUI für startmenü

--- GUI.py
+++ GUI.py
@@ -1,58 +1,27 @@
 import tkinter as tk
-from tkinter import messagebox
+from subprocess import call
 
-class TicTacToe:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Tic Tac Toe")
+def button1_clicked():
+    call(["python", "Hangman_V2.py"])
 
-        self.current_player = 'X'
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+def button2_clicked():
+    call(["python", "tictactoe.py"])
 
-        self.buttons = [[tk.Button(root, text=' ', font=('normal', 20), width=6, height=2,
-                                   command=lambda row=i, col=j: self.on_click(row, col))
-                         for j in range(3)] for i in range(3)]
+def button3_clicked():
+    call(["python", "SchereSteinPapier.py"])
 
-        for i in range(3):
-            for j in range(3):
-                self.buttons[i][j].grid(row=i, column=j)
 
-    def on_click(self, row, col):
-        if self.board[row][col] == ' ':
-            self.board[row][col] = self.current_player
-            self.buttons[row][col].config(text=self.current_player, state=tk.DISABLED)
+root = tk.Tk()
+root.geometry("300x150")
+root.title("Spielesammlung DHBW Bad Mergentheim")
 
-            if self.check_winner():
-                messagebox.showinfo("Game Over", f"Player {self.current_player} wins!")
-                self.reset_game()
-            elif self.is_board_full():
-                messagebox.showinfo("Game Over", "It's a tie!")
-                self.reset_game()
-            else:
-                self.current_player = 'O' if self.current_player == 'X' else 'X'
+button1 = tk.Button(root, text="Hangman", command=button1_clicked)
+button1.pack(pady=10)
 
-    def check_winner(self):
-        for i in range(3):
-            if all(self.board[i][j] == self.current_player for j in range(3)) or \
-               all(self.board[j][i] == self.current_player for j in range(3)):
-                return True
+button2 = tk.Button(root, text="tictactoe", command=button2_clicked)
+button2.pack(pady=10)
 
-        if all(self.board[i][i] == self.current_player for i in range(3)) or \
-           all(self.board[i][2 - i] == self.current_player for i in range(3)):
-            return True
+button3 = tk.Button(root, text="Schere Stein Paar Bier", command=button3_clicked)
+button3.pack(pady=10)
 
-        return False
-
-    def is_board_full(self):
-        return all(self.board[i][j] != ' ' for i in range(3) for j in range(3))
-
-    def reset_game(self):
-        for i in range(3):
-            for j in range(3):
-                self.board[i][j] = ' '
-                self.buttons[i][j].config(text=' ', state=tk.NORMAL)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    game = TicTacToe(root)
-    root.mainloop()
+root.mainloop()
