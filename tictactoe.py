6b7c936ec95ae3f1eab20816421d1257967413ad
Robin Schuch <rob.schuch.23@lehre.mosbach.dhbw.de>
2024-02-26 16:01:16 +0100

    smaller changes

--- tictactoe.py
+++ tictactoe.py
@@ -14,9 +14,9 @@ class Board:
 
     def check_win(self, symbol):
         winning_combinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontal
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertical
-            [0, 4, 8], [2, 4, 6]             # Diagonal
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+            [0, 4, 8], [2, 4, 6]             
         ]
         for combo in winning_combinations:
             if all(self.state[i] == symbol for i in combo):
